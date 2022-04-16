b34a04daefb5189a135bfae5b9f07e2a45de8ced
erikas-taroza <erikastaroza@gmail.com>
2022-04-15 22:09:28 -0700

    Always generate log

--- logger.py
+++ logger.py
@@ -1,27 +1,16 @@
 import time
 
 class Logger:
-    # If the file doesn't exist, then that means we don't want logging.
-    can_log = False
-    try:
-        export = open("./log.txt", "r")
-        if export != None:
-            # Switch to write mode
-            export.close()
-            export = open("./log.txt", "a")
-            export.write("\n********************************************************\n")
-            (year, month, month_day, hour, minute, second, week_day, year_day, is_dst) = time.localtime()
-            export.write(f"Log for: {month}/{month_day}/{year} {hour}:{minute}")
-            export.write("\n********************************************************\n")
-            export.close()
-            can_log = True
-    except FileNotFoundError:
-        pass
+    file = open("./log.txt", "a")
+    file.write("\n********************************************************\n")
+    (year, month, month_day, hour, minute, second, week_day, year_day, is_dst) = time.localtime()
+    file.write(f"Log for: {month}/{month_day}/{year} {hour}:{minute}")
+    file.write("\n********************************************************\n")
+    file.close()
 
     def log(message, print_to_console = False):
-        if Logger.can_log:
-            export = open("./log.txt", "a")
-            export.write(message + "\n")
-            export.close()
+        file = open("./log.txt", "a")
+        file.write(message + "\n")
+        file.close()
         if print_to_console:
             print(message)
